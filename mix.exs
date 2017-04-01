aab29c283f4cb7be8aff5cd22ea0b18599e01ed9
Andrew Forward <aforward@gmail.com>
2017-04-01 07:38:14 -0400

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule StripePost.Mixfile do
 
   @git_url "https://github.com/aforward/stripe-post"
   @home_url @git_url
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [app: :stripe_post,
